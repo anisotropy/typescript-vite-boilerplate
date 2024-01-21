5fd4abc9869e08a6e770a7811f8d840dddce565b
anisotropy <j.hodoug@gmail.com>
2024-01-21 18:28:21 +0900

    Update .eslintrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,9 +3,6 @@
 module.exports = {
   extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint', 'stratified-design'],
-  rules: {
-    'stratified-design/no-same-level-funcs': ['error'],
-  },
+  plugins: ['@typescript-eslint'],
   root: true,
 }
